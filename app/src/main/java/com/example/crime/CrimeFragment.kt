2bc2b8fdcde136c08046ccbbee62aa9c8bf78252
it-zavadskiy <123456>
2020-02-10 16:47:52 +0300

    Добавлено диалоговое окно

--- app/src/main/java/com/example/crime/CrimeFragment.kt
+++ app/src/main/java/com/example/crime/CrimeFragment.kt
@@ -8,17 +8,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.CompoundButton
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import kotlinx.android.synthetic.main.fragment_crime.*
 import kotlinx.android.synthetic.main.fragment_crime.view.*
 import java.util.*
 import androidx.core.widget.addTextChangedListener as addTextChangedListener1
 
 class CrimeFragment : Fragment(R.layout.fragment_crime) {
-   val DIALOG_DATE = "DialogDate"
+    val DIALOG_DATE = "DialogDate"
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var mCrime = Crime()
-       val crimeId = arguments?.getSerializable(ARG_CRIME_ID) as Int
+        val crimeId = arguments?.getSerializable(ARG_CRIME_ID) as Int
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -28,12 +29,16 @@ class CrimeFragment : Fragment(R.layout.fragment_crime) {
 
             override fun afterTextChanged(s: Editable) {}
 
-            override fun beforeTextChanged(s: CharSequence, start: Int,
-                                           count: Int, after: Int) {
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
             }
 
-            override fun onTextChanged(s: CharSequence, start: Int,
-                                       before: Int, count: Int) {
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
                 mCrime.mTitle = s.toString()
             }
         })
@@ -42,18 +47,24 @@ class CrimeFragment : Fragment(R.layout.fragment_crime) {
 
         crime_date.text = mCrime.mDate.toString()
         crime_date.setOnClickListener {
+            var manager: FragmentManager? = fragmentManager
+            var dialog:DatePickerFragment = DatePickerFragment()
+            if (manager != null) {
+                dialog.show(manager,DIALOG_DATE)
+            }
 
         }
         crime_solved.setOnCheckedChangeListener { _, isChecked -> mCrime.mSolved = isChecked }
     }
-companion object {
-    val ARG_CRIME_ID="crime_id"
-     fun newInstance(crimeId: Int) : CrimeFragment{
-       var args=Bundle()
-         args.putSerializable(ARG_CRIME_ID,crimeId)
-         var fragment=CrimeFragment()
-         fragment.arguments = args
-         return fragment
+
+    companion object {
+        val ARG_CRIME_ID = "crime_id"
+        fun newInstance(crimeId: Int): CrimeFragment {
+            var args = Bundle()
+            args.putSerializable(ARG_CRIME_ID, crimeId)
+            var fragment = CrimeFragment()
+            fragment.arguments = args
+            return fragment
+        }
     }
-}
 }
